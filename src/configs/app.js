6c8db3a500110939d559144cd88860566fe3fe3c
HYUNNSOOSHIN <shs0655@gmail.com>
2020-03-28 22:59:12 +0900

    heroku api server 연결

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,7 +1,7 @@
 const app = {
   url: {
-    // api: 'https://soolog.herokuapp.com',
-    api: 'http://127.0.0.1:3000',
+    api: 'https://soolog.herokuapp.com',
+    // api: 'http://127.0.0.1:3000',
   }
 }
 
